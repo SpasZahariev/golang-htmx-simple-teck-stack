65ffd448be3611b573060fa10aba60c7c915ea89
spas.z <spaszahariev54@gmail.com>
2024-10-08 19:34:43 +0200

    using the existing vpc and subnets... now it magically works in aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,101 +7,101 @@ resource "aws_ecs_cluster" "loc_cluster" {
 
 }
 # Setup public subnet that has a route to an internet gateway
-resource "aws_vpc" "loc_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.loc_vpc.id
-}
-
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.loc_vpc.id
-  cidr_block              = "10.0.2.0/24" # Different CIDR block
-  map_public_ip_on_launch = true
-  availability_zone       = "eu-north-1a" # Specify the availability zone
-}
-
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.loc_vpc.id
-  cidr_block              = "10.0.3.0/24" # Different CIDR block
-  map_public_ip_on_launch = true
-  availability_zone       = "eu-north-1b" # Different availability zone
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.loc_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "public_rt_assoc_1" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "public_rt_assoc_2" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-# security group setup
-resource "aws_security_group" "alb_sg" {
-  name        = "alb-sg"
-  description = "Allow HTTP traffic"
-  # vpc_id      = "vpc-07c18ba2d9821b698"
-  vpc_id = aws_vpc.loc_vpc.id
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "ecs_sg" {
-  name        = "ecs-sg"
-  description = "Allow traffic from ALB"
-  # vpc_id      = "vpc-07c18ba2d9821b698"
-  vpc_id = aws_vpc.loc_vpc.id
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# resource "aws_vpc" "loc_vpc" {
+#   cidr_block = "10.0.0.0/16"
+# }
+#
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.loc_vpc.id
+# }
+#
+# resource "aws_subnet" "public_subnet_1" {
+#   vpc_id                  = aws_vpc.loc_vpc.id
+#   cidr_block              = "10.0.2.0/24" # Different CIDR block
+#   map_public_ip_on_launch = true
+#   availability_zone       = "eu-north-1a" # Specify the availability zone
+# }
+#
+# resource "aws_subnet" "public_subnet_2" {
+#   vpc_id                  = aws_vpc.loc_vpc.id
+#   cidr_block              = "10.0.3.0/24" # Different CIDR block
+#   map_public_ip_on_launch = true
+#   availability_zone       = "eu-north-1b" # Different availability zone
+# }
+#
+# resource "aws_route_table" "public_rt" {
+#   vpc_id = aws_vpc.loc_vpc.id
+#
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
+# }
+#
+# resource "aws_route_table_association" "public_rt_assoc_1" {
+#   subnet_id      = aws_subnet.public_subnet_1.id
+#   route_table_id = aws_route_table.public_rt.id
+# }
+#
+# resource "aws_route_table_association" "public_rt_assoc_2" {
+#   subnet_id      = aws_subnet.public_subnet_2.id
+#   route_table_id = aws_route_table.public_rt.id
+# }
+#
+# # security group setup
+# resource "aws_security_group" "alb_sg" {
+#   name        = "alb-sg"
+#   description = "Allow HTTP traffic"
+#   # vpc_id      = "vpc-07c18ba2d9821b698"
+#   vpc_id = aws_vpc.loc_vpc.id
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+#
+# resource "aws_security_group" "ecs_sg" {
+#   name        = "ecs-sg"
+#   description = "Allow traffic from ALB"
+#   # vpc_id      = "vpc-07c18ba2d9821b698"
+#   vpc_id = aws_vpc.loc_vpc.id
+#
+#   ingress {
+#     from_port       = 80
+#     to_port         = 80
+#     protocol        = "tcp"
+#     security_groups = [aws_security_group.alb_sg.id]
+#   }
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 
 # task definition setup
@@ -111,8 +111,6 @@ resource "aws_ecs_task_definition" "loc_task" {
     {
       name      = "loc-container"
       image     = "054037109154.dkr.ecr.eu-north-1.amazonaws.com/first-namespace/first-repo:latest"
-      cpu       = 256
-      memory    = 512
       essential = true
       portMappings = [
         {
@@ -128,14 +126,23 @@ resource "aws_ecs_task_definition" "loc_task" {
           appProtocol   = "http"
         }
       ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/loc-task"
+          awslogs-region        = "eu-north-1"
+          awslogs-create-group  = "true"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     }
   ])
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = var.execution_role_arn
   # task_role_arn            = var.task_role_arn
   network_mode = "awsvpc"
-  cpu          = "256"
-  memory       = "512"
+  cpu          = 256
+  memory       = 512
 
   runtime_platform {
     operating_system_family = "LINUX"
@@ -147,18 +154,18 @@ resource "aws_lb" "loc_alb" {
   name               = var.alb_name
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  # security_groups = ["sg-04d502a839c7338ec"]
-  # subnets = ["subnet-0eb6b3d96b986165e", "subnet-0e77ab66e0751c549"]
-  subnets = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  # security_groups    = [aws_security_group.alb_sg.id]
+  # subnets = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  security_groups = ["sg-04d502a839c7338ec"]
+  subnets         = ["subnet-0eb6b3d96b986165e", "subnet-0e77ab66e0751c549"]
 }
 
 resource "aws_lb_target_group" "loc_target_group" {
   name     = "loc-target-group"
   port     = 80
   protocol = "HTTP"
-  # vpc_id      = "vpc-07c18ba2d9821b698"
-  vpc_id      = aws_vpc.loc_vpc.id
+  vpc_id   = "vpc-07c18ba2d9821b698"
+  # vpc_id      = aws_vpc.loc_vpc.id
   target_type = "ip"
   health_check {
     path = "/"
@@ -181,15 +188,28 @@ resource "aws_ecs_service" "loc_service" {
   name            = "loc-ecs-service"
   cluster         = aws_ecs_cluster.loc_cluster.id
   task_definition = aws_ecs_task_definition.loc_task.arn
-  launch_type     = "FARGATE"
+  # launch_type     = "FARGATE"
+  # platform_version = "1.3.0" # Specify the desired platform version
+
 
   desired_count = 1
 
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    weight            = 1
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 1
+  }
+
   network_configuration {
-    # subnets         = ["subnet-0eb6b3d96b986165e", "subnet-0e77ab66e0751c549"]
-    subnets         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
-    security_groups = [aws_security_group.ecs_sg.id]
-    # security_groups = ["sg-04d502a839c7338ec"]
+    subnets = ["subnet-0eb6b3d96b986165e", "subnet-0e77ab66e0751c549"]
+    # subnets         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+    # security_groups = [aws_security_group.ecs_sg.id]
+    security_groups  = ["sg-04d502a839c7338ec"]
+    assign_public_ip = true # Enable public IP assignment
   }
 
   load_balancer {
