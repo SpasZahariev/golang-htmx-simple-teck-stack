354c6b3721b8c4e647cf350a14f9948b4893292d
spas.z <spaszahariev54@gmail.com>
2024-10-08 20:28:55 +0200

    defining my own subnets still works

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,98 +10,105 @@ resource "aws_ecs_cluster" "loc_cluster" {
 # resource "aws_vpc" "loc_vpc" {
 #   cidr_block = "10.0.0.0/16"
 # }
-#
+
 # resource "aws_internet_gateway" "igw" {
-#   vpc_id = aws_vpc.loc_vpc.id
+#   # vpc_id = aws_vpc.loc_vpc.id
+#   vpc_id = var.og_vpc_id
 # }
-#
-# resource "aws_subnet" "public_subnet_1" {
-#   vpc_id                  = aws_vpc.loc_vpc.id
-#   cidr_block              = "10.0.2.0/24" # Different CIDR block
-#   map_public_ip_on_launch = true
-#   availability_zone       = "eu-north-1a" # Specify the availability zone
-# }
-#
-# resource "aws_subnet" "public_subnet_2" {
-#   vpc_id                  = aws_vpc.loc_vpc.id
-#   cidr_block              = "10.0.3.0/24" # Different CIDR block
-#   map_public_ip_on_launch = true
-#   availability_zone       = "eu-north-1b" # Different availability zone
-# }
-#
-# resource "aws_route_table" "public_rt" {
-#   vpc_id = aws_vpc.loc_vpc.id
-#
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
-# }
-#
-# resource "aws_route_table_association" "public_rt_assoc_1" {
-#   subnet_id      = aws_subnet.public_subnet_1.id
-#   route_table_id = aws_route_table.public_rt.id
-# }
-#
-# resource "aws_route_table_association" "public_rt_assoc_2" {
-#   subnet_id      = aws_subnet.public_subnet_2.id
-#   route_table_id = aws_route_table.public_rt.id
-# }
-#
-# # security group setup
-# resource "aws_security_group" "alb_sg" {
-#   name        = "alb-sg"
-#   description = "Allow HTTP traffic"
-#   # vpc_id      = "vpc-07c18ba2d9821b698"
-#   vpc_id = aws_vpc.loc_vpc.id
-#   ingress {
-#     from_port   = 8080
-#     to_port     = 8080
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-#
-# resource "aws_security_group" "ecs_sg" {
-#   name        = "ecs-sg"
-#   description = "Allow traffic from ALB"
-#   # vpc_id      = "vpc-07c18ba2d9821b698"
-#   vpc_id = aws_vpc.loc_vpc.id
-#
-#   ingress {
-#     from_port       = 80
-#     to_port         = 80
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.alb_sg.id]
-#   }
-#   ingress {
-#     from_port   = 8080
-#     to_port     = 8080
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+
+resource "aws_subnet" "public_subnet_1" {
+  # vpc_id                  = aws_vpc.loc_vpc.id
+  vpc_id = var.og_vpc_id
+  # cidr_block              = "10.0.2.0/24" # Different CIDR block
+  cidr_block              = "172.31.48.0/20" # Different CIDR block
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-north-1a" # Specify the availability zone
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  # vpc_id                  = aws_vpc.loc_vpc.id
+  vpc_id = var.og_vpc_id
+  # cidr_block              = "10.0.3.0/24" # Different CIDR block
+  cidr_block              = "172.31.64.0/20" # Different CIDR block
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-north-1b" # Different availability zone
+}
+
+resource "aws_route_table" "public_rt" {
+  # vpc_id = aws_vpc.loc_vpc.id
+  vpc_id = var.og_vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    # gateway_id = aws_internet_gateway.igw.id
+    gateway_id = var.igw_id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# security group setup
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = var.og_vpc_id
+  # vpc_id = aws_vpc.loc_vpc.id
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs-sg"
+  description = "Allow traffic from ALB"
+  vpc_id      = var.og_vpc_id
+  # vpc_id = aws_vpc.loc_vpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 # task definition setup
@@ -154,17 +161,17 @@ resource "aws_lb" "loc_alb" {
   name               = var.alb_name
   internal           = false
   load_balancer_type = "application"
-  # security_groups    = [aws_security_group.alb_sg.id]
-  # subnets = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
-  security_groups = ["sg-04d502a839c7338ec"]
-  subnets         = ["subnet-0eb6b3d96b986165e", "subnet-0e77ab66e0751c549"]
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  # security_groups = ["sg-04d502a839c7338ec"]
+  # subnets         = ["subnet-0eb6b3d96b986165e", "subnet-0e77ab66e0751c549"]
 }
 
 resource "aws_lb_target_group" "loc_target_group" {
   name     = "loc-target-group"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "vpc-07c18ba2d9821b698"
+  vpc_id   = var.og_vpc_id
   # vpc_id      = aws_vpc.loc_vpc.id
   target_type = "ip"
   health_check {
@@ -205,8 +212,8 @@ resource "aws_ecs_service" "loc_service" {
   }
 
   network_configuration {
-    subnets = ["subnet-0eb6b3d96b986165e", "subnet-0e77ab66e0751c549"]
-    # subnets         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+    # subnets = ["subnet-0eb6b3d96b986165e", "subnet-0e77ab66e0751c549"]
+    subnets = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
     # security_groups = [aws_security_group.ecs_sg.id]
     security_groups  = ["sg-04d502a839c7338ec"]
     assign_public_ip = true # Enable public IP assignment
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,3 +10,11 @@ variable "alb_name" {
 variable "execution_role_arn" {
   default = "arn:aws:iam::054037109154:role/ecsTaskExecutionRole"
 }
+
+variable "og_vpc_id" {
+  default = "vpc-07c18ba2d9821b698"
+}
+
+variable "igw_id" {
+  default = "igw-0e85d67dbe2658e2f"
+}
